e3fced8cf1a60423ab2364213eabb765307fecdb
fontesrafa <rafaelfontes777@hotmail.com>
2022-02-11 11:43:32 -0300

    Created Client Entity

---
+++ src/main/java/com/github/fontesrafa/Entities/Client.java
@@ -0,0 +1,14 @@
+package com.github.fontesrafa.Entities;
+
+public class Client {
+    
+    public String fullName;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
