9c308d74405e9bb8bbc86426b1480cbdcb79b701
fontesrafa <rafaelfontes777@hotmail.com>
2022-02-10 22:08:21 -0300

    Created Bank Project

---
+++ src/main/java/com/github/fontesrafa/Main.java
@@ -0,0 +1,13 @@
+package com.github.fontesrafa;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Test!" );
+    }
+}
